290f322f893ba5968cee42fdab305778ea74fc2d
Doreen <doreenmarie_cee@hotmail.com>
2023-09-15 22:28:41 +0200

    creating class and properties for the Race Car iteration

--- js/game.js
+++ js/game.js
@@ -1,3 +1,45 @@
 class Game {
     // code to be added
+    constructor () {
+        this.startScreen = document.getElementById(`game-intro`);
+        this.gameScreen = document.getElementById(`game-screen`);
+        this.gameEndScreen = document.getElementById(`game-end`);
+        this.player = null;
+        this.height = 600;
+        this.width = 500;
+        this.obstacles = [];
+        this.score = 0;
+        this.lives = 3;
+        this.gameIsOver = false;
+    }
+
+    start (){
+        this.hideElements(this.startScreen);
+        this.showElements(this.gameScreen);
+
+    }
+
+
+
+
+    gameLoop (){
+
+    }
+    
+    
+
+
+    
+    update(){
+
+
+
+
+
+    }
+
+
+
+
+
 }
